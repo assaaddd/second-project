fca43e4bc57daa7a77ab665f1a1797b434a9373a
Asadbek <asadbeeksafaraliyev@gmail.com>
2025-04-25 18:17:50 +0800

    create sign in api route

--- api/controllers/auth_controller.js
+++ api/controllers/auth_controller.js
@@ -1,16 +1,36 @@
 import bcryptjs from 'bcryptjs';
+import jwt from 'jsonwebtoken';
 
 import User from '../models/user.model.js';
 import { errorHandler } from '../utils/error.js';
 
-export const signup = async(req, res, next) => {
-    try{
-        const {username , email , password} = req.body;
+export const signup = async (req, res, next) => {
+    try {
+        const { username, email, password } = req.body;
         const hashedPassword = await bcryptjs.hash(password, 10)
-        const newUser = new User({username, email, password: hashedPassword});
+        const newUser = new User({ username, email, password: hashedPassword });
         await newUser.save();
         res.json(newUser);
-    }catch(err){
+    } catch (err) {
         next(err);
+    }
+}
+
+export const signIn = async (req, res, next) => {
+    const { email, password } = req.body;
+    try {
+        const validUser = await User.findOne({ email });
+        if (!validUser) return next(errorHandler(404, 'User not found'));
+        const validPassword = await bcryptjs.compare(password, validUser.password);
+        if (!validPassword) return next(errorHandler(401, 'Wrong credentials!'));
+        const token = jwt.sign({ id: validUser._id }, process.env.JWT_SECRET);
+        const { password: pass, ...rest } = validUser._doc;
+
+        res
+            .cookie('accos_token', token, { httpOnly: true })
+            .status(200)
+            .json(rest);
+    } catch (err) {
+        next(err)
     }
 }
--- api/routes/auth_route.js
+++ api/routes/auth_route.js
@@ -2,10 +2,12 @@ import express from 'express';
 
 import {
     signup,
+    signIn
 } from '../controllers/auth_controller.js';
 
 const router = express.Router();
 
 router.post('/signup' , signup);
+router.post('/signin' , signIn);
 
 export default router;
